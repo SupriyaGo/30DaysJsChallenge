99f4e91cde57526a9f5035665c7ad7699ac57bdb
Supriya Gorai <supriyagorai1995@gmail.com>
2024-07-17 23:48:46 +0530

    comments added

--- Day1/A_Day1.js
+++ Day1/A_Day1.js
@@ -3,16 +3,16 @@
 // Activity 1
 // Task 1
 var num = 3;
-console.log("Variable Number", num);
+console.log("Variable Number", num); // Output: 3
 
 // Task 2
 let str = "Supriyo";
-console.log("Variable string", str);
+console.log("Variable string", str); // Output: Supriyo
 
 // Activity 2
 // Task 3
 const bool = true;
-console.log("Variable Boolean", bool);
+console.log("Variable Boolean", bool); // Output: true
 
 // Activity 3
 // Task 4
@@ -21,22 +21,22 @@ const strn = "Supriyo";
 const booll = true;
 const obj = { key: "value" };
 const arr = ["1", "2", "3"];
-console.log("Type: ", typeof numb);
-console.log("Type: ", typeof strn);
-console.log("Type: ", typeof booll);
-console.log("Type: ", typeof obj);
-console.log("Type: ", typeof ["1", "2", "3"]);
+console.log("Type: ", typeof numb); // Output: number
+console.log("Type: ", typeof strn); // Output: string
+console.log("Type: ", typeof booll); // Output: boolean
+console.log("Type: ", typeof obj); // Output: object
+console.log("Type: ", typeof ["1", "2", "3"]); // Output: object
 
 // Activity 4
 // Task 5
 let var1 = 5;
-console.log("Initial value", var1);
+console.log("Initial value", var1); // Output: 5
 var1 = 6;
-console.log("New Value", var1);
+console.log("New Value", var1); // Output: 6
 
 // Activity 5
 // Task 6
 const var2 = 3;
-console.log("Initial const value", var2);
+console.log("Initial const value", var2); // Output: 3
 var2 = 10;
-console.log("New const Value", var2);
+console.log("New const Value", var2); // Output: TypeError: Assignment to constant variable.
