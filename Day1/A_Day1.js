3517fd1de5373b5db2cf42f8b34022799d909d8e
Supriya Gorai <supriyagorai1995@gmail.com>
2024-07-17 23:44:41 +0530

    day 1 task

---
+++ Day1/A_Day1.js
@@ -0,0 +1,42 @@
+/** @format */
+
+// Activity 1
+// Task 1
+var num = 3;
+console.log("Variable Number", num);
+
+// Task 2
+let str = "Supriyo";
+console.log("Variable string", str);
+
+// Activity 2
+// Task 3
+const bool = true;
+console.log("Variable Boolean", bool);
+
+// Activity 3
+// Task 4
+const numb = 3;
+const strn = "Supriyo";
+const booll = true;
+const obj = { key: "value" };
+const arr = ["1", "2", "3"];
+console.log("Type: ", typeof numb);
+console.log("Type: ", typeof strn);
+console.log("Type: ", typeof booll);
+console.log("Type: ", typeof obj);
+console.log("Type: ", typeof ["1", "2", "3"]);
+
+// Activity 4
+// Task 5
+let var1 = 5;
+console.log("Initial value", var1);
+var1 = 6;
+console.log("New Value", var1);
+
+// Activity 5
+// Task 6
+const var2 = 3;
+console.log("Initial const value", var2);
+var2 = 10;
+console.log("New const Value", var2);
